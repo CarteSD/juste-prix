d030c837ea8a353b308f355a40fdd431dd20ad87
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-02-27 13:30:13 +0100

    :sparkles: Création de la requête d'initialisation de partie

--- src/server.js
+++ src/server.js
@@ -5,13 +5,12 @@ import config from '../config.json' with { type: 'json' };
 import path from 'path';
 import { dirname } from 'path';
 import {fileURLToPath} from "url";
+import {JustePrix} from "./game/JustePrix.class.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-
-
 const server = createServer(app);
 const io = new Server(server);
 
@@ -21,6 +20,40 @@ const games = new Map();
 // Constantes uniques pour l'ensemble des parties
 const MIN_PLAYERS = 2;
 const MAX_PLAYERS = 10;
+
+// Route pour initialiser une partie
+app.post('/:gameId/init', express.json(), (req, res) => {
+
+    const gameId = req.params.gameId;
+    const { settings, players } = req.body;
+    if (players.length > MAX_PLAYERS) {
+        res.status(409).json({
+            success: false,
+            message: 'Trop de joueurs pour la partie'
+        });
+        return;
+    }
+    if (players.length < MIN_PLAYERS) {
+        res.status(409).json({
+            success: false,
+            message: 'Pas assez de joueurs pour la partie'
+        });
+        return;
+    }
+    try {
+        games.set(gameId, new JustePrix(gameId, settings.nbRounds, players));
+        res.status(200).json({
+            success: true,
+            message: 'Partie initialisée avec succès'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: 'Erreur lors de l\'initialisation de la partie'
+        });
+    }
+});
 
 // Route d'erreur 404 (page introuvable)
 app.get('/404', (req, res) => {
