f022e00814e113c3cda1afacc65b28a64fa43604
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-03-10 11:57:34 +0100

    :lipstick: Suppression du contenu du champ de saisi lors du début de manche

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -102,6 +102,7 @@ socket.on('new round', ({roundNumber}) => {
     sendBtn.disabled = false;
     sendBtn.classList.remove("btn-disabled");
     sendBtn.classList.add("btn-primary");
+    messageInput.value = "";
     messageInput.focus();
     messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
 });
